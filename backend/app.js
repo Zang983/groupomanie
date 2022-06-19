32b0f5b9ecc1e738150dfda6cba6967cf1979eef
KERKAR Kevin <kerkar.kevin@gmail.com>
2022-06-19 23:02:38 +0200

    ajout de différents modules de sécurité, fin css accueil pour le mobile

--- backend/app.js
+++ backend/app.js
@@ -4,11 +4,13 @@ const ceRoutes= require('./routes/messageCE');
 const userRoutes = require('./routes/user');
 const commentRoutes=require('./routes/comments')
 const path = require('path');
-const cookieParser = require('cookie-parser')
+const helmet = require("helmet")
+const cors = require("cors")
 
 
 
 const app = express();//création de la fonction express permettant de créer le serveur.
+
 
 
 app.use((req, res, next) => {
@@ -17,9 +19,12 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   next();
 });
-app.use(express.json());//permet d'écouter les requêtes avec un content-type JSON cela le met dans req.body
 
-app.use(cookieParser())
+
+app.use(express.json());//permet d'écouter les requêtes avec un content-type JSON cela le met dans req.body
+app.use(helmet());
+app.use(cors());
+
 app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use('/api/posts', postsRoutes);
 app.use('/api/auth/', userRoutes);
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -3,9 +3,19 @@ const router = express.Router();
 const userCtrl = require('../controllers/user');
 const auth = require('../middleware/auth')
 const multer = require('../middleware/multer-config')
+const rateLimit = require("express-rate-limit")
+
+/*On limite les tentatives de connexion à 15 en 15 minutes*/
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, 
+	max: 15, 
+	standardHeaders: true, 
+	legacyHeaders: false, 
+})
+
 
 router.post('/signup', userCtrl.signup);
-router.post('/login', userCtrl.login);
+router.post('/login',limiter, userCtrl.login);
 router.get('/profil/:id',auth,userCtrl.profil);
 router.put('/parametre/:id',auth,multer,userCtrl.parametre);
 router.delete('/delete/:id',auth,userCtrl.delete)
